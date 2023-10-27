1cb161885835b789089c57a1172bf42abee52d53
Marc Anderson <marcda495@gmail.com>
2023-10-27 11:05:07 -0400

    'na'

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { authStore, type UserData } from "../stores/authStore";
+  import { doc, getDoc, setDoc } from "firebase/firestore";
+  import { auth, db } from "$lib/firebase/firebase.client";
+
 
   let notchLeft = 0,
     notchRight = 0,
@@ -14,6 +18,48 @@
   onMount(() => {
     screen.orientation.onchange = handleOrientation;
     handleOrientation();
+
+    // update authStore on authentication state changes
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      // logged out
+      if (!user) {
+        // update authStore
+        authStore.update((curr) => {
+          return {
+            ...curr,
+            isLoading: false,
+            currentUser: user,
+          };
+        });
+        return;
+      }
+
+      // get firestore document data
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      let dataToSetStoreTo: UserData = {text:""};
+
+      // create a new user doc if it doesn't exist
+      if (!docSnap.exists()) {
+        console.log("Creating firestore user doc...");
+        const userRef = doc(db, "users", user.uid);
+        await setDoc(userRef, dataToSetStoreTo, { merge: true });
+      } else {
+        console.log("Fetching firestore user doc...");
+        const userData = docSnap.data();
+        dataToSetStoreTo = userData as UserData;
+      }
+
+      // update authStore
+      authStore.update((curr) => {
+        return {
+          ...curr,
+          isLoading: false,
+          currentUser: user,
+          data: dataToSetStoreTo,
+        };
+      });
+    });
   });
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,8 @@
 
 <h1>Home</h1>
 
+<a href="/App">
+  <button>
+    <img src="/images/logo.svg" alt="Home" style="width:1em" />
+  </button>
+</a>
