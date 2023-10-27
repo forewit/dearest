7b975edbdf29e25ebe43c00b272e0a0c74a75f20
Marc Anderson <marcda495@gmail.com>
2023-10-27 10:32:14 -0400

    'na'

--- src/lib/Components/VerticalSlider.svelte
+++ src/lib/Components/VerticalSlider.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
+  // The slider is "springy" and bounces to the
+  // closest position while being pulled to the finger
+  // using neumorphic design (see neumorphism.io)
 
-    // The slider is "springy" and bounces to the
-    // closest position while being pulled to the finger
-    // using neumorphic design (see neumorphism.io)
-    
-    
-    type Option = {
-        position: number // 0-100
-        icon?: string, //url
-    }
-    
-    let options: Option[] = [];
-    </script>
-    
-    
-    {#each options as option}
-        
-    {/each}
-    
-    
-    <style>
-    
-    
-    </style>
+  type Option = {
+    position: number; // 0-100
+    icon?: string; //url
+  };
+
+  let options: Option[] = [];
+</script>
+
+{#each options as option}{/each}
+
+<style>
+</style>
