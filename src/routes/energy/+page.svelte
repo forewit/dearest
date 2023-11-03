911e96d92484a2fc702641ea20836a3c21d61bd9
Marc Anderson <marcda495@gmail.com>
2023-11-03 11:58:03 -0400

    'na'

--- src/routes/energy/+page.svelte
+++ src/routes/energy/+page.svelte
@@ -1,28 +1,25 @@
 <script lang="ts">
-let value = 4;
+  let value = 4;
 </script>
 
-
 <div class="grid-container center">
-    <img src="/images/energy/energy{value}.svg" alt="">
-    <input class="range" type="range" min="1" max="5" bind:value />
+  <img src="/images/energy/energy{value}.svg" alt="" />
+  <input class="range" type="range" min="1" max="5" bind:value />
 </div>
 
-
 <style>
-
-input[type="range"]::-webkit-slider-thumb {
+  input[type="range"]::-webkit-slider-thumb {
     -webkit-mask: url("/images/energy.svg") no-repeat center / contain;
     mask: url("/images/energy.svg") no-repeat center / contain;
   }
-    .grid-container {
-        display: grid;
-        grid-auto-flow: row;
-        gap: 40px;
-    }
-img {
+  .grid-container {
+    display: grid;
+    grid-auto-flow: row;
+    gap: 40px;
+  }
+  img {
     height: 150px;
     align-self: center;
     justify-self: center;
-}
+  }
 </style>
